0bb0b72474223718e855fee95a742ea7f5b47b24
nguyentuandung <dungnt@dxmb.vn>
2016-03-25 09:26:56 +0700

    [dxmb] IOS core
    First install source code for datxanhmienbac.vn

---
+++ datxanhmienbac,vn/datxanhmienbac,vn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  datxanhmienbac,vn
+//
+//  Created by Macbook on 25/03/2016.
+//  Copyright © 2016 DXMB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
